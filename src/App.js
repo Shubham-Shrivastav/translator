0baea04e8c52b33897a5dec8ebc297b99c3ac81c
Shubham-Shrivastav <shuboom@969@gmail.com>
2023-05-03 15:04:15 +0530

    Initial commit of a basic structure of a translator with added axios lib

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+const axios = require('axios').default; //after installed the npm add axios we have to acquire this ..
 
 function App() {
+  const [options, setOptions] = useState([]);
+  const [to, setTo] = useState("en");
+  const [from, setFrom] = useState("en");
+  const [input, setInput] = useState("en");
+  const [output, setOutput] = useState("en");
+
+  useEffect(() => {
+    axios.get('https://libretranslate.de/languages',
+      { headers: { 'accept': 'application/json' } }).then(res => {
+        console.log(res.data);
+        setOptions(res.data);
+      })
+  }, [])
+
+  //curl -X 'GET' \'https://libretranslate.de/languages' \-H 'accept: application/json'
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        From ({from}) :
+        <select onChange={e => setFrom(e.target.value)}>
+          {options.map(opt => <option key={opt.code} value={opt.code}> {opt.name} </option>)}
+        </select>
+        To ({to}):
+        <select onChange={e => setTo(e.target.value)}>
+          {options.map(opt => <option key={opt.code} value={opt.code}> {opt.name} </option>)}
+        </select>
+      </div>
+      <div>
+        <textarea cols="50" rows="8"></textarea>
+      </div>
+      <div>
+        <textarea cols="50" rows="8"></textarea>
+      </div>
+      <div>
+        <button> Translate </button>
+      </div>
     </div>
   );
 }
